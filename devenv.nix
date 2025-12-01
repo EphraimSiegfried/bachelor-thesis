7144dc9c018bc8443e87a1b921419b7629e5418e
Ephraim Siegfried <ephraim.siegfried@proton.me>
2025-12-01 11:01:22 +0100

    update devenv

--- devenv.nix
+++ devenv.nix
@@ -1,6 +1,16 @@
-{ config, ... }:
+{ config, pkgs, ... }:
 {
-  languages.typst.enable = true;
+  languages.typst = {
+    enable = true;
+    fontPaths = [
+      "${pkgs.cascadia-code}"
+    ];
+  };
+
   env.TYPST_ROOT = "${config.env.DEVENV_ROOT}/src";
   scripts.compile.exec = "typst compile $DEVENV_ROOT/src/main.typ bsc_thesis_siegfried.pdf";
+
+  packages = with pkgs; [
+    cascadia-code
+  ];
 }
