5dd037276a575091b13920d87596b7032ecf4334
Ephraim Siegfried <ephraim.siegfried@proton.me>
2025-12-13 13:57:56 +0100

    fix wrong timestamp

--- devenv.nix
+++ devenv.nix
@@ -9,7 +9,7 @@
   };
 
   env.TYPST_ROOT = "${config.env.DEVENV_ROOT}/src";
-  scripts.compile.exec = "typst compile $DEVENV_ROOT/src/main.typ bsc_thesis_siegfried.pdf";
+  scripts.compile.exec = "typst compile --creation-timestamp=$(date +%s) $DEVENV_ROOT/src/main.typ bsc_thesis_siegfried.pdf";
 
   packages = with pkgs; [
     cascadia-code
