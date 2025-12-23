6d21ccc99696cbff33a4e32f6d52aa1fdeb2124b
Ephraim Siegfried <ephraim.siegfried@proton.me>
2025-12-23 14:43:00 +0100

    add scientific integrity

--- devenv.nix
+++ devenv.nix
@@ -9,10 +9,13 @@
   };
 
   env.TYPST_ROOT = "${config.env.DEVENV_ROOT}/src";
-  scripts.compile.exec = "typst compile --creation-timestamp=$(date +%s) $DEVENV_ROOT/src/main.typ bsc_thesis_siegfried.pdf";
+  scripts.compile_typst.exec = "typst compile --creation-timestamp=$(date +%s) $DEVENV_ROOT/src/main.typ out.pdf";
+  scripts.compile.exec = "compile_typst;pdftk out.pdf src/content/scientific_integrity.pdf cat output bsc_thesis_siegfried.pdf; rm out.pdf
+";
 
   packages = with pkgs; [
     cascadia-code
     corefonts
+    pdftk
   ];
 }
