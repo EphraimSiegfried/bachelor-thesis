bed6a63e6298b688556dbf97a708022a7021697f
Ephraim Siegfried <ephraim.siegfried@proton.me>
2025-12-26 16:52:19 +0100

    improve script name

--- devenv.nix
+++ devenv.nix
@@ -9,8 +9,8 @@
   };
 
   env.TYPST_ROOT = "${config.env.DEVENV_ROOT}/src";
-  scripts.compile_typst.exec = "typst compile --creation-timestamp=$(date +%s) $DEVENV_ROOT/src/main.typ out.pdf";
-  scripts.compile.exec = "compile_typst && pdftk out.pdf src/content/scientific_integrity.pdf cat output bsc_thesis_siegfried.pdf && rm out.pdf
+  scripts.compile_thesis.exec = "typst compile --creation-timestamp=$(date +%s) $DEVENV_ROOT/src/main.typ out.pdf";
+  scripts.compile.exec = "compile_thesis && pdftk out.pdf src/content/scientific_integrity.pdf cat output bsc_thesis_siegfried.pdf && rm out.pdf
 ";
 
   packages = with pkgs; [
